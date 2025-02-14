7694fc6fa733474305ccd379c9eca6a01cec551e
Nur Zawani Ainin Zabidi <zawaniainin03@gmail.com>
2025-02-15 02:21:30 +0800

    Update and rename model.sql to student-profile.sql

--- student-profile/database/student-profile.sql
+++ student-profile/database/student-profile.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Nov 09, 2017 at 12:39 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +9,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `model`
+-- Database: `student-profile`
 --
 
 -- --------------------------------------------------------
@@ -46,20 +38,16 @@ CREATE TABLE IF NOT EXISTS `student` (
   `student_id` varchar(220) NOT NULL,
   `name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `report` varchar(2000) NOT NULL,
-  `yoa` varchar(45) NOT NULL,
-  `parent` varchar(200) NOT NULL,
   `dob` date NOT NULL,
   `gender` varchar(7) NOT NULL,
+  `admission_year` varchar(45) NOT NULL,
+  `faculty` varchar(200) NOT NULL,
+  `course` varchar(200) NOT NULL,
+  `semester` varchar(200) NOT NULL,
   `file` varchar(400) NOT NULL
+
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`id`, `student_id`, `name`, `last_name`, `report`, `yoa`, `parent`, `dob`, `gender`, `file`) VALUES
-(1, 'MS17-e32c', 'Taola', 'Remilekun', 'She is a bad girl... run away from her!', '2012', '08138652645', '1993-05-05', 'Female', 'your_site_name_4b7720e1d717573bf412a49b43537063.png');
 
 -- --------------------------------------------------------
 
